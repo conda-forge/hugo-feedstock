d1a950249892d2465f0ce56a07864eaefce317fa
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-11-09 19:38:28 +0530

    Correctly skip across platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,21 +4,18 @@ set -ex
 
 GO_VERSION="1.23.3"
 
-# Determine OS and architecture for Go download
 # Set empty GOROOT for Linux builds based on
 # https://stackoverflow.com/a/57892698
 if [[ "$target_platform" == "linux-ppc64le" ]]; then
     GO_TARBALL="go${GO_VERSION}.linux-ppc64le.tar.gz"
     export GOROOT=""
+    # Download and install external Go
+    curl -LJO "https://go.dev/dl/${GO_TARBALL}"
+    mkdir -p ${PREFIX}/go_installed
+    tar -C ${PREFIX}/go_installed -xzf ${GO_TARBALL}
+    export PATH=${PREFIX}/go_installed/go/bin:${PATH}
+    go version
 fi
-
-# Download and install external Go
-curl -LJO "https://go.dev/dl/${GO_TARBALL}"
-mkdir -p ${PREFIX}/go_installed
-tar -C ${PREFIX}/go_installed -xzf ${GO_TARBALL}
-export PATH=${PREFIX}/go_installed/go/bin:${PATH}
-go version
-
 
 # Build Hugo
 export GO111MODULE=on
