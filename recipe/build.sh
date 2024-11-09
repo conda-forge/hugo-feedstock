71a6f5f96ac1d0f9edc7fd2dcff926478a6d49c4
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-11-09 22:51:46 +0530

    Remove `--ignore` command, undo `GOROOT` removal

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,6 @@ GO_VERSION="1.23.3"
 # https://stackoverflow.com/a/57892698
 if [[ "$target_platform" == "linux-ppc64le" ]]; then
     GO_TARBALL="go${GO_VERSION}.linux-ppc64le.tar.gz"
-    export GOROOT=""
     # Download and install external Go
     curl -LJO "https://go.dev/dl/${GO_TARBALL}"
     mkdir -p ${PREFIX}/go_installed
@@ -24,11 +23,11 @@ export CGO_ENABLED=1
 cd $SRC_DIR
 go build -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -tags extended,withdeploy -trimpath -v -o $PREFIX/bin/hugo
 if [[ "$target_platform" == "linux-ppc64le" ]]; then
-    # Use v1 for go-licenses that stillcontains support for non go-mod
+    # Use v1 for go-licenses that still contains support for non go-mod
     # based projects: https://github.com/google/go-licenses/issues/128
     # Note: most of the errors are coming from the standard library
     # such as os, net, etc.; they are not too relevant to the build.
-    go run github.com/google/go-licenses@v1.0.0 save . --save_path ./library_licenses --ignore github.com/BurntSushi/locker
+    go run github.com/google/go-licenses@v1.0.0 save . --save_path ./library_licenses
 else
     go-licenses save . --save_path ./library_licenses
 fi
