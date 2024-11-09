dd157559601bf7bb515a2b7b1015990c1ea23372
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-11-09 17:50:59 +0530

    Use MSI installer for Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,11 @@
 @echo on
 
+:: Download and install Go via MSI installer
+curl -LJO https://go.dev/dl/go1.23.3.windows-amd64.msi || exit 1
+start "Install Go MSI" /wait msiexec.exe /i go1.23.3.windows-amd64.msi TARGETDIR=%PREFIX% /quiet || exit 1
+go version || exit 1
+
+:: Build Hugo
 set GO111MODULE=on
 set CGO_ENABLED=1
 
