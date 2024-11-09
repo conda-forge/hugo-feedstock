01a889ea61a0e96b11bd35064d2f7d94e580e385
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-11-09 19:28:54 +0530

    Use old version of go-licenses on Linux-ppc64le

--- recipe/build.sh
+++ recipe/build.sh
@@ -26,7 +26,15 @@ export CGO_ENABLED=1
 
 cd $SRC_DIR
 go build -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -tags extended,withdeploy -trimpath -v -o $PREFIX/bin/hugo
-go-licenses save . --save_path ./library_licenses
+if [[ "$target_platform" == "linux-ppc64le" ]]; then
+    # Use v1 for go-licenses that stillcontains support for non go-mod
+    # based projects: https://github.com/google/go-licenses/issues/128
+    # Note: most of the errors are coming from the standard library
+    # such as os, net, etc.; they are not too relevant to the build.
+    go run https://github.com/google/go-licenses@v1.0.0 save . --save_path ./library_licenses
+else
+    go-licenses save . --save_path ./library_licenses
+fi
 chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
 
 # Clean up Go installation files
