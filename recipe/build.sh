0b41b41784df627d7b03ae7e5910de80e5227eb8
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-11-09 17:51:22 +0530

    Use tarballs for Linux and macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,40 @@
 
 set -ex
 
+GO_VERSION="1.23.3"
+
+# Determine OS and architecture for Go download
+case "$target_platform" in
+    linux-64)
+        GO_TARBALL="go${GO_VERSION}.linux-amd64.tar.gz"
+        ;;
+    linux-aarch64)
+        GO_TARBALL="go${GO_VERSION}.linux-arm64.tar.gz"
+        ;;
+    linux-ppc64le)
+        GO_TARBALL="go${GO_VERSION}.linux-ppc64le.tar.gz"
+        ;;
+    osx-64)
+        GO_TARBALL="go${GO_VERSION}.darwin-amd64.tar.gz"
+        ;;
+    osx-arm64)
+        GO_TARBALL="go${GO_VERSION}..darwin-arm64.tar.gz"
+        ;;
+    *)
+        echo "Unsupported platform: $target_platform"
+        exit 1
+        ;;
+esac
+
+# Download and install external Go
+curl -LJO "https://go.dev/dl/${GO_TARBALL}"
+mkdir -p ${PREFIX}/go_installed
+tar -C ${PREFIX}/go_installed -xzf ${GO_TARBALL}
+export PATH=${PREFIX}/go_installed/go/bin:${PATH}
+go version
+
+
+# Build Hugo
 export GO111MODULE=on
 export CGO_ENABLED=1
 
@@ -9,3 +43,6 @@ cd $SRC_DIR
 go build -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -tags extended -trimpath -v -o $PREFIX/bin/hugo
 go-licenses save . --save_path ./library_licenses
 chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
+
+# Clean up Go installation files
+rm -f "${GO_TARBALL}"
