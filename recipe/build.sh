6553352c53a3dfe5736c1baf4d149dd39f57e7f7
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-11-09 17:52:10 +0530

    Enable withdeploy feature

--- recipe/build.sh
+++ recipe/build.sh
@@ -40,7 +40,7 @@ export GO111MODULE=on
 export CGO_ENABLED=1
 
 cd $SRC_DIR
-go build -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -tags extended -trimpath -v -o $PREFIX/bin/hugo
+go build -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -tags extended,withdeploy -trimpath -v -o $PREFIX/bin/hugo
 go-licenses save . --save_path ./library_licenses
 chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
 
