b8db246371d11f0f71ed88e364e456fb0f80cf5a
Uwe L. Korn <uwe.korn@quantco.com>
2025-09-09 09:39:09 +0200

    Remove some cruft

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-export GO111MODULE=on
-export CGO_ENABLED=1
-
 cd $SRC_DIR
 go build -ldflags "-s -w -X main.revision=conda-forge -X github.com/gohugoio/hugo/common/hugo.vendorInfo=conda-forge" -tags extended -trimpath -v -o $PREFIX/bin/hugo
 go-licenses save . --save_path ./library_licenses
